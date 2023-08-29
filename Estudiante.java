cb167cca068966a7dbd20603ea520d1e23c2cdb1
Yahir Zapata <yahirzapatae@gmail.com>
2023-08-29 07:34:25 -0600

    Classes and Menu Package

--- Estudiante.java
+++ Estudiante.java
@@ -5,16 +5,11 @@ public class Estudiante {
     public String telefono;
     public String semestre;
 
-     Estudiante(){
-        System.out.println("Objeto estudiante, creado");
-    }
      Estudiante(String nombre, String correo){
-        this.nombre = nombre;
-        this.correo = correo;
-    }
-
-    public void mostrarNombre(){
-        System.out.println("El nombre del estudiante es: " + nombre);
-        System.out.println("Correo: " + correo);
-    }
+         this.nombre = nombre;
+         this.correo = correo;
+     }
+     public void mostrarNombre(){
+         System.out.println("Me llamo: " + nombre);
+     }
 }
